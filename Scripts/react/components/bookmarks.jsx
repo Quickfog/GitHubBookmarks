f8b208702faa9535f7ede524fbf6cf0d444973f9
Quickfog <Quickfog@FOGS7>
2019-06-23 21:45:46 +0300

    Add project files.

---
+++ Scripts/react/components/bookmarks.jsx
@@ -0,0 +1,83 @@
+﻿class NameForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { bookmarks: [] };
+        this.loadBookmarks = this.loadBookmarks.bind(this);
+        this.resetBookmarks = this.resetBookmarks.bind(this);
+    }
+
+    loadBookmarks() {
+        axios.get("/Bookmarks/Get").then(response => {
+            this.setState({ bookmarks: [] });
+            response.data.map((value, key) => {
+                const temp = this.state.bookmarks.slice()
+                temp.push(value)
+                this.setState({ bookmarks: temp });
+            });
+            console.log(this.state.bookmarks);
+        });
+    }
+
+
+    resetBookmarks() {
+        axios.post("/Bookmarks/Reset").then(response => {
+            if (response.data === "success") {
+                this.setState({ bookmarks: [] });
+            }
+        });
+    }
+
+    componentDidMount() {
+        this.loadBookmarks();
+    }
+
+    render() {
+        return (
+            <div className="row">
+                {this.state.bookmarks.length ? (
+                    <div className="col-12 mt-5">
+                        <h1 className="display-3">Your Bookmarked Repositories</h1>
+                        <button id="deleteRepos" onClick={this.resetBookmarks} type="button" className="btn btn-danger bmd-btn-fab">
+                            <i className="material-icons">delete</i>
+                        </button>
+                    </div>
+                ) : '' }
+                {this.state.bookmarks.length ? (
+                    this.state.bookmarks.map((repo) => {
+                        return (
+                            <div className="col-xl-3 mb-3 mt-3">
+                                <div className="card" key={repo.id}>
+                                    <div className="card-body">
+                                        <div className="d-flex justify-content-start align-items-center">
+                                            <img className="card-img-top" src={repo.Owner.Avatar_url} />
+                                            <h5 className="card-title">{repo.Name}</h5>
+                                        </div>
+                                        <p className="card-text mt-3">Owner: {repo.Owner.Login}</p>
+                                        <p className="card-text mt-3">Description: {repo.Description}</p>
+                                        <p className="card-text">Languge: {repo.Language}</p>
+                                        <div className="d-flex justify-content-between align-items-center card-actions">
+                                            <div className="d-flex justify-content-between align-items-center repo-stats pl-0 col-6">
+                                                <span><i className="fas fa-star"></i><span className="pl-1">{repo.StargazersCount}</span></span>
+                                                <span><i className="fas fa-eye"></i><span className="pl-1">{repo.Watchers}</span></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                ) : (<div className="d-flex text-center flex-column p-4 m-auto">
+                    <h1 className="display-2">No Bookmarks Yet</h1>
+                    <h2>Please Add Bookmarks First</h2>
+                    <a href="/" className="btn btn-raised btn-primary">Back</a>
+                </div>)}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <NameForm />,
+    document.getElementById('app')
+);
---
+++ Scripts/react/components/index.jsx
@@ -0,0 +1,109 @@
+﻿class NameForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: '',
+            repos: [],
+            validationError: false,
+            isLoading: false,
+            pageNum: '1'
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.bookmarkRepo = this.bookmarkRepo.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ searchTerm: event.target.value });
+    }
+    /**
+     * 
+     * @param {string}
+     * Make a get request to the Github API and map the returned results to state.repos.
+     */
+    handleSubmit(event) {
+        event.preventDefault();
+        if (this.state.searchTerm === "") {
+            this.setState({ validationError: true });
+        } else {
+            this.setState({ validationError: false });
+            axios.get("https://api.github.com/search/repositories?q=" + this.state.searchTerm + "&page=" + this.state.pageNum)
+                .then(response => {this.setState({ repos: [] });
+                response.data.items.map((key, value) => {
+                    const temp = this.state.repos.slice()
+                    temp.push(key)
+                    this.setState({ repos: temp });
+                });
+            });
+        }
+    }
+    /**
+     * 
+     * @param {any} event
+     * 
+     */
+    bookmarkRepo(event) {
+        event.preventDefault();
+        let repo = this.state.repos.find((repo) => {
+            return repo.id == event.target.id
+        });
+        axios.post('Home/Store', { "data": repo }, ).then(response => {
+            if (response.data === "success") {
+                const temp = this.state.repos.filter((repository) => {
+                    return repository.id !== repo.id
+                })
+                this.setState({ repos: temp });
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit} className="mt-5 form-inline col-12">
+                    <header className="mb-3">
+                        <h1 className="display-3">Search Github Repositorie's</h1>
+                        <span className="bmd-form-group">
+                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value={this.state.value} onChange={this.handleChange} />
+                            <button className="btn btn-outline-secondery my-2 my-sm-0" type="submit"><i className="material-icons">search</i></button>
+                        </span>
+                        {this.state.validationError ? <span className="text-danger d-flex flex-column">Please Type in at least 1 charecter</span> : ''}
+                    </header>
+                </form>
+                <div className="row">
+                    {this.state.repos.map((repo) => {
+                        return (
+                            <div className="col-xl-3 mb-3 mt-3" key={repo.id}>
+                                <div className="card">
+                                    <div className="card-body">
+                                        <div className="d-flex justify-content-start align-items-center">
+                                            <img className="card-img-top" src={repo.owner.avatar_url} />
+                                            <h5 className="card-title">{repo.name}</h5>
+                                        </div>
+                                        <p className="card-text mt-3">Owner: {repo.owner.login}</p>
+                                        <p className="card-text mt-3">Description: {repo.description}</p>
+                                        <p className="card-text">Languge: {repo.language}</p>
+                                        <div className="d-flex justify-content-between align-items-center card-actions">
+                                            <div className="d-flex justify-content-between align-items-center repo-stats pl-0 col-6">
+                                                <span><i className="fas fa-star"></i><span className="pl-1">{repo.stargazers_count}</span></span>
+                                                <span><i className="fas fa-eye"></i><span className="pl-1">{repo.watchers}</span></span>
+                                            </div>
+                                            <button id={repo.id} onClick={this.bookmarkRepo} className="btn btn-raised btn-primary">Bookmark</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <NameForm />,
+    document.getElementById('app')
+);
